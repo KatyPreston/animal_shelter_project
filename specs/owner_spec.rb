f3dcf936446b940b0d68639a083fe03977c3577d
KatyPreston <prestonkj@hotmail.com>
2018-05-07 12:42:41 +0100

    owner test added

---
+++ specs/owner_spec.rb
@@ -0,0 +1,21 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../models/adoption")
+require_relative("../models/animal")
+require_relative("../models/owner")
+
+class AdoptionTest < Minitest::Test
+
+  def setup
+    @animal1 = Animal.new("name" =>"Monkey", "type" => "Guinea Pig", "admission_date" => "May 2018", "adoptable" => true)
+    @owner1 = Owner.new("name" => "Laura")
+
+    @adoption = Adoption.new("animal_id" => @animal1_id, "owner_id" =>@owner1.id)
+  end
+
+  def test_owner_name
+    assert_equal("Laura", @owner1.name)
+  end
+
+
+end
