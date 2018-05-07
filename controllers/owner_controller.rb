6f929a1bbe3897c15eb1b6d242ceb991df5362ba
KatyPreston <prestonkj@hotmail.com>
2018-05-07 13:26:40 +0100

    file structure reorganised

---
+++ controllers/owner_controller.rb
@@ -0,0 +1,6 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative("./models/adoption")
+require_relative("./models/animal")
+require_relative("./models/owner")
